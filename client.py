241a727a3dcdd0d5286fa533cee8968573088ab3
Vi <viwang@Vis-MacBook-Pro.local>
2023-04-24 15:02:27 +0100

    test

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import requests
+from time import sleep
 
-for i in range(1):
-    r = requests.get("http://127.0.0.1:8000/param")
+for i in range(1000):
+    r = requests.get("http://127.0.0.1:5000/trace_endpoint")
     print(r.text)
+    sleep(30)
+    
