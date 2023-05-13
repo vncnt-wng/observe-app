d34fecab3f46ff43ee703774fd17c25f9ad7325b
Vi <viwang@Vis-MacBook-Pro.local>
2023-05-13 18:20:24 +0100

    Adds trace context propagation for python

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-
 from flask import Flask, request
 from time import sleep
 from opentelemetry.trace.status import StatusCode
 from other_file import some_function
-from trace_utils import trace_function, trace
+from trace_utils import trace_function, trace, add_trace_headers
+import random
+import requests
+
+# from opentelemetry.instrumentation.flask import FlaskInstrumentor
 # from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 # # from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 # from opentelemetry.sdk.resources import SERVICE_NAME, Resource
@@ -46,10 +49,10 @@ def err():
         span.set_status(StatusCode.ERROR)
 
 
-@app.route("/param", methods=['GET', 'POST'])
+@app.route("/param", methods=["GET", "POST"])
 def param():
-    arg1 = request.args.get('arg1', default=1, type=int)
-    arg2 = request.args.get('arg2', default=1, type=int)
+    arg1 = request.args.get("arg1", default=1, type=int)
+    arg2 = request.args.get("arg2", default=1, type=int)
     print(request.data)
     for i in range(arg1):
         func2(arg1)
@@ -63,8 +66,7 @@ def hello():
 
     span.set_attribute("set_attribute", "value")
 
-    span.add_event("event message",
-                   {"event_attributes": 1})
+    span.add_event("event message", {"event_attributes": 1})
 
     # with tracer.start_as_current_span("in1") as span2:
     #     sleep(30 / 100)
@@ -80,7 +82,7 @@ def hello():
     return "Hello World\n"
 
 
-class Test():
+class Test:
     @trace_function
     def func(self):
         return "aaa"
@@ -105,24 +107,24 @@ def called(arg1, arg2):
 @app.route("/trace_endpoint")
 @trace_function
 def endpoint():
-    sleep(30/1000)
+    sleep(30 / 1000)
     return func1(1, 2)
 
 
 @trace_function
 def func1(arg1, arg2):
-    sleep(30/1000)
+    sleep(30 / 1000)
     return func2(3)
-
 
 
 @trace_function
 def func2(arg1):
-    sleep(30/1000)
+    sleep(30 / 1000)
     return "return"
 
-@trace_function
+
 @app.route("/accross_file")
+@trace_function
 def across_file():
     some_function()
     return "ok"
@@ -131,59 +133,112 @@ def across_file():
 @app.route("/flame_test")
 @trace_function
 def flame_test():
-    sleep(10/1000)
-    sub1()
-    sub2()
+    sleep(10 / 1000)
+    if random.randint(0, 1) == 0:
+        sub1()
+    if random.randint(0, 1) == 0:
+        sub2()
     return "ok"
+
 
 @trace_function
 def sub1():
     subsub1()
-    subsub2() 
-    
+    subsub2()
+
+
 @trace_function
 def sub2():
-    sleep(5/1000)
-    subsub3() 
-    
+    sleep(5 / 1000)
+    subsub3()
+
+
 @trace_function
 def subsub1():
-    sleep(8/1000)
-    
+    sleep(8 / 1000)
+
+
 @trace_function
 def subsub2():
-    sleep(5/1000)
-    
+    sleep(5 / 1000)
+
+
 @trace_function
 def subsub3():
-    sleep(10/1000)
-    
-    
+    sleep(10 / 1000)
+
+
+@app.route("/multiple_execution_path")
+@trace_function
+def multiple_execution_path():
+    value = random.randint(0, 1)
+    if value == 0:
+        path1()
+    else:
+        path2()
+    return str(value)
+
+
+@trace_function
+def path1():
+    sleep(0.01)
+    pass
+
+
+@trace_function
+def path2():
+    sleep(0.01)
+    pass
+
+
 class Example:
     def __init__(self):
         self.attr1 = 5
         self.attr2 = "string"
-        self.attr3 = [1,2,3]
-        self.attr4 = {"a":1, "b":2}
+        self.attr3 = [1, 2, 3]
+        self.attr4 = {"a": 1, "b": 2}
         self.attr5 = Example2()
 
     def func(self):
         pass
 
+
 class Example2:
     def __init__(self):
         self.attr1 = "string"
 
+
 @app.route("/parameter_test")
 @trace_function
 def parameter_test():
-    parameter_called(0, 0.1, "hello", [1,2,3], {1,2}, {"a": 1, "b": 2}, Example())
+    parameter_called(0, 0.1, "hello", [1, 2, 3], {1, 2}, {"a": 1, "b": 2}, Example())
     return "hello"
+
 
 @trace_function
 def parameter_called(i, f, str, l, s, d, o):
-    return 
+    return
+
+
+@app.route("/distributed")
+@trace_function
+def distributed():
+    return "ok"
+
+
+@app.route("/caller")
+@trace_function
+def caller():
+    headers = add_trace_headers({})
+    r = requests.get("http://127.0.0.1:5000/callee", headers=headers)
+    return "ok"
+
+
+@app.route("/callee")
+@trace_function
+def callee():
+    return "ok"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
- 
--- trace_utils.py
+++ trace_utils.py
@@ -1,38 +1,49 @@
-from typing import Tuple
-import os 
-import inspect 
+from typing import Tuple, Dict, overload, Callable, TypeVar, ParamSpec
+import os
+import inspect
 import subprocess
 import re
 import json
-
+from functools import wraps
+from flask import request
 from opentelemetry import trace
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 
 
 def get_git_root() -> str:
-    completed_process = subprocess.run(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE)
+    completed_process = subprocess.run(
+        ["git", "rev-parse", "--show-toplevel"], stdout=subprocess.PIPE
+    )
     # [:-1] to remove endline
-    return completed_process.stdout.decode("utf-8")[:-1] + '/'
+    return completed_process.stdout.decode("utf-8")[:-1] + "/"
+
 
 """
 Returns the current commit, branch and message if available, else None
 """
+
+
 def parse_git_reflog() -> Tuple[str, str, str]:
     # TODO ensure this parsing is sufficient
     """
-    reflog of the form: 
+    reflog of the form:
     af7ab5b (HEAD -> b2, master) HEAD@{0}: checkout: moving from master to b2
     af7ab5b (HEAD -> b2, master) HEAD@{1}: commit: test
     ba89296 (b1) HEAD@{2}: commit (initial): .
     """
     branch_name = check_git_repository()
     if branch_name != None:
-        stdout_lines = subprocess.run(['git', 'reflog', '--decorate'], stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
+        stdout_lines = (
+            subprocess.run(["git", "reflog", "--decorate"], stdout=subprocess.PIPE)
+            .stdout.decode("utf-8")
+            .split("\n")
+        )
         for line in stdout_lines:
-            substrings = line.split(': ')
-            if substrings[1] == 'commit':
-                commit_details = re.split(r'\(|\)', substrings[0])
+            substrings = line.split(": ")
+            if substrings[1] == "commit":
+                commit_details = re.split(r"\(|\)", substrings[0])
                 commit_id = commit_details[0][:-1]
-                branches = commit_details[1].split('-> ')[1].split(', ')
+                branches = commit_details[1].split("-> ")[1].split(", ")
                 message = substrings[-1]
                 # the first commit entry should start with current branch
                 if branches[0] == branch_name:
@@ -40,74 +51,134 @@ def parse_git_reflog() -> Tuple[str, str, str]:
                     return commit_id, branch, message
         return None, None, None
 
+
 """
 Checks if a git repo exists, returns branch name if so or empty string
-"""        
+"""
+
+
 def check_git_repository() -> str:
-    completed_process = subprocess.run(['git', 'status'], stdout=subprocess.PIPE)
+    completed_process = subprocess.run(["git", "status"], stdout=subprocess.PIPE)
     if completed_process.returncode == 0:
-        return completed_process.stdout.decode('utf-8').split('\n')[0].split('On branch ')[1]
+        return (
+            completed_process.stdout.decode("utf-8")
+            .split("\n")[0]
+            .split("On branch ")[1]
+        )
     return None
+
 
 git_root = get_git_root()
 tracer = trace.get_tracer(__name__)
 commit_id, branch, message = parse_git_reflog()
 
 
-
 # dumps a class object to json (shallow)
 def dump_obj_to_json(obj):
     data = {}
-    data['type'] = "class"
+    data["type"] = "class"
     data["className"] = type(obj).__name__
     values = obj.__dict__
     for key in values.keys():
         typeObj = type(values[key])
-        # if attribute is an object, just record the class name 
-        if '__dict__' in typeObj.__dict__:
+        # if attribute is an object, just record the class name
+        if "__dict__" in typeObj.__dict__:
             values[key] = {"class": typeObj.__name__}
-    data['values'] = values
+    data["values"] = values
     return json.dumps(data)
+
+
+def header_from_carrier(carrier, key):
+    header = carrier.get(key)
+    return [header] if header else []
+
+
+def get_header_from_flask_request(request, key):
+    return request.headers.get_all(key)
+
 
 """
 Wrapper function that starts a new otel trace on a function, recording function details as attributes
 """
+
+
 def trace_function(func):
     def wrapper(*args, **kwargs):
-        with tracer.start_as_current_span(f"{func.__name__}") as span:
+        ctx = None
+        try:
+            traceparent = get_header_from_flask_request(request, "traceparent")
+            carrier = {"traceparent": traceparent[0]}
+            ctx = TraceContextTextMapPropagator().extract(carrier)
+        except:
+            pass
+
+        print("context")
+        print(ctx)
+
+        with tracer.start_as_current_span(f"{func.__name__}", context=ctx) as span:
+            try:
+                print(request.headers)
+                if (
+                    "traceparent" in request.headers
+                    and "callerSpanId" in request.headers
+                ):
+                    print("here")
+                    span.set_attribute("callerSpanId", request.headers["callerSpanId"])
+            except:
+                print("error")
+                pass
+
             absolute_path = os.path.abspath(inspect.getfile(func))
-            span.set_attribute(f"file", absolute_path.replace(git_root, ''))
+            span.set_attribute(f"file", absolute_path.replace(git_root, ""))
             qualname = func.__qualname__
             span.set_attribute(f"qualName", qualname)
 
             # If function in a class, don't add 'self' arg to trace attributes
             arg_start = 0
-            if len(qualname.split('.')) > 1:
+            if len(qualname.split(".")) > 1:
                 arg_start = 1
-                
+
             func_args = list(inspect.signature(func).parameters.keys())
             for n in range(arg_start, len(args)):
-                print('__dict__' in type(args[n]).__dict__)
-                print(type(args[n]).__name__ )
-                if '__dict__' in type(args[n]).__dict__:
+                print("__dict__" in type(args[n]).__dict__)
+                print(type(args[n]).__name__)
+                if "__dict__" in type(args[n]).__dict__:
                     span.set_attribute(func_args[n], dump_obj_to_json(args[n]))
                     # args_data[func_args[n]] = dump_obj_to_json(args[n])
-                elif type(args[n]).__name__ == 'dict':
-                    span.set_attribute(func_args[n], json.dumps({'type': 'dict', 'value': args[n]}))
-                elif type(args[n]).__name__ == 'set' :
-                    span.set_attribute(func_args[n], json.dumps({'type': 'set', 'value': list(args[n])}))
-                elif type(args[n]).__name__ == 'list' :
-                    span.set_attribute(func_args[n], json.dumps({'type': 'list', 'value': args[n]}))
+                elif type(args[n]).__name__ == "dict":
+                    span.set_attribute(
+                        func_args[n], json.dumps({"type": "dict", "value": args[n]})
+                    )
+                elif type(args[n]).__name__ == "set":
+                    span.set_attribute(
+                        func_args[n],
+                        json.dumps({"type": "set", "value": list(args[n])}),
+                    )
+                elif type(args[n]).__name__ == "list":
+                    span.set_attribute(
+                        func_args[n], json.dumps({"type": "list", "value": args[n]})
+                    )
                 else:
                     span.set_attribute(func_args[n], args[n])
                     # args_data[func_args[n]] = args[n]
-                    
-                
-            if branch: 
+
+            if branch:
                 span.set_attribute("commit_id", commit_id)
                 span.set_attribute("branch", branch)
                 span.set_attribute("message", message)
-            
+
             return func(*args, **kwargs)
+
     wrapper.__name__ = func.__name__
     return wrapper
+
+
+# Adds trace headers to a header dictionary
+def add_trace_headers(headers: Dict):
+    carrier = {}
+    TraceContextTextMapPropagator().inject(carrier)
+    # For W3c trace context convention
+    headers["traceparent"] = carrier["traceparent"]
+    headers["callerSpanId"] = hex(trace.get_current_span().get_span_context().span_id)
+    print(headers)
+    return headers
